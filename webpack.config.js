7dcb9781286377c169b7333f1572b6e09220ca84
Mohammad Mokhtari <dev.mohammadmokhtari@gmail.com>
2021-09-15 23:07:58 +0430

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,98 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const CopyPlugin = require('copy-webpack-plugin');
+
+// let mode = 'development';
+// let target = 'web';
+// if(process.env.NODE_ENV ===  'production'){
+//   mode = 'production';
+// target = 'browserslist'
+// }
+
+module.exports = {
+  mode: 'development',
+  target: 'web',
+  entry: {
+    'js/app': './src/js/app.js',
+    'css/style.bundel': './src/sass/style.scss',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist', 'assets'),
+    filename: '[name].bundel.js',
+    // assetModuleFilename: 'images/[hash][ext][query]'
+    // publicPath: '/dist/assets/',
+  },
+  devServer: {
+    compress: true,
+    open: true,
+    hot: true,
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  { useBuiltIns: 'usage' /*corejs: { version: 3 }*/ },
+                ],
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'resolve-url-loader', // add this before sass-loader
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(ttf|eot|woff|woff2)$/,
+        exclude: /node_modules/,
+        include: [/fonts/],
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[hash][ext][query]',
+        },
+      },
+      {
+        test: /\.(gif|png|jpe?g)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[hash][ext][query]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanPlugin.CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: './src/media/images', to: './images' }],
+    }),
+  ],
+};
---
+++ webpack.config.prod.js
@@ -0,0 +1,91 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  target: 'browserslist',
+  entry: {
+    'js/app': './src/js/app.js',
+    'css/style.bundel': './src/sass/style.scss',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist', 'assets'),
+    filename: '[name].bundel.js',
+    // assetModuleFilename: 'images/[hash][ext][query]'
+    // publicPath: '/dist/assets/',
+  },
+  devServer: {
+    compress: true,
+    open: true,
+    hot: true,
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  { useBuiltIns: 'usage' /*corejs: { version: 3 }*/ },
+                ],
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'resolve-url-loader', // add this before sass-loader
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(ttf|eot|woff|woff2)$/,
+        exclude: /node_modules/,
+        include: [/fonts/],
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[hash][ext][query]',
+        },
+      },
+      {
+        test: /\.(gif|png|jpe?g)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[hash][ext][query]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanPlugin.CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: './src/media/images', to: './images' }],
+    }),
+  ],
+};
